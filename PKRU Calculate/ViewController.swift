d145678d87825aa8cef5de4006c4c85a4663d77c
PKRU02 <PKRU02@pkru02s-imac.local>
2017-07-25 15:35:16 +0700

    After Barek

--- PKRU Calculate/ViewController.swift
+++ PKRU Calculate/ViewController.swift
@@ -9,17 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var answerOutlet: UILabel!
+    @IBOutlet weak var number1Outlet: UITextField!
+    @IBOutlet weak var number2Outlet: UITextField!
+    @IBAction func calculateButton(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+    } // Main Method
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
+    } // Second Method
 
 
-}
+} // Main Class
 
